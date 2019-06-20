54facb036196935d686bebc07ae86c53e4c4880b
Tung Nguyen <tongueroo@gmail.com>
2019-06-20 01:21:15 +0000

    change codebuild interface to be simpler and more flexible

--- lib/codepipe/dsl/pipeline/codebuild.rb
+++ lib/codepipe/dsl/pipeline/codebuild.rb
@@ -31,26 +31,33 @@ module Codepipe::Dsl::Pipeline
       action(props.reverse_merge(default))
     end
 
-    def codebuild(*args)
-      props = args.last.is_a?(Hash) ? args.pop : {}
-      names = args # codebuild project names
+    def codebuild(*projects)
+      default = {
+        # name: '', # will be set
+        action_type_id: {
+          category: "Build",
+          owner: "AWS",
+          provider: "CodeBuild",
+          version: "1",
+        },
+        run_order: @run_order,
+        # configuration: { project_name: '' }, # will be set
+        # output_artifacts: [name: "BuildArtifact#{name}"], # TODO: maybe make this configurable with a setting
+        input_artifacts: [name: "SourceArtifact"],
+      }
 
-      actions = names.map do |name|
-        project_name = get_project_name(name)
-        default = {
-          name: name.underscore.camelize,
-          action_type_id: {
-            category: "Build",
-            owner: "AWS",
-            provider: "CodeBuild",
-            version: "1",
-          },
-          run_order: @run_order,
-          configuration: { project_name: project_name },
-          # output_artifacts: [name: "BuildArtifact#{name}"], # TODO: maybe make this configurable with a setting
-          input_artifacts: props[:input_artifacts] || [name: "SourceArtifact"],
-        }
-        props.reverse_merge(default)
+      actions = projects.map do |item|
+        if item.is_a?(String)
+          name = item.underscore.camelize
+          project_name = get_project_name(item)
+          default.deep_merge(
+            name: name,
+            configuration: { project_name: project_name },
+          )
+        else # Hash
+          # With the hash, the user needs to set: name and configuration.project_name
+          item.reverse_merge(default)
+        end
       end
 
       action(*actions)
