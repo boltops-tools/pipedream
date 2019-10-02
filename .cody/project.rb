06694702fc07c4c8829ff3dadf7bb3e3c741447b
Tung Nguyen <tongueroo@gmail.com>
2019-10-02 00:53:40 +0000

    update repo

--- .cody/project.rb
+++ .cody/project.rb
@@ -1,4 +1,4 @@
-github_url("https://github.com/tongueroo/codepipeline")
+github_url("https://github.com/tongueroo/pipedream")
 linux_image("aws/codebuild/ruby:2.5.3-1.7.0")
 triggers(webhook: true)
 local_cache(false)
