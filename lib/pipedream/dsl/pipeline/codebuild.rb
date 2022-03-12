ca169cbd67fa750ca325c3bdb076edf2d0abe578
Tung Nguyen <tongueroo@gmail.com>
2022-03-12 21:35:13 +0000

    camelcase codebuild dsl internals and add PrimarySource support

--- lib/pipedream/dsl/pipeline/codebuild.rb
+++ lib/pipedream/dsl/pipeline/codebuild.rb
@@ -2,37 +2,39 @@ module Pipedream::Dsl::Pipeline
   module Codebuild
     def codebuild(*projects)
       default = {
-        # name: '', # will be set
-        action_type_id: {
-          category: "Build",
-          owner: "AWS",
-          provider: "CodeBuild",
-          version: "1",
+        # Name: '', # will be set
+        ActionTypeId: {
+          Category: "Build",
+          Owner: "AWS",
+          Provider: "CodeBuild",
+          Version: "1",
         },
-        run_order: @run_order,
-        # configuration: { project_name: '' }, # will be set
-        # output_artifacts: [name: "BuildArtifact#{name}"], # TODO: maybe make this configurable with a setting
-        input_artifacts: [name: "MainArtifact"],
+        RunOrder: @run_order,
+        # Configuration: { ProjectName: '' }, # will be set
+        # OutputArtifacts: [Name: "BuildArtifact#{name}"], # TODO: maybe make this configurable with a setting
+        InputArtifacts: [Name: "MainArtifact"],
       }
 
       actions = projects.map do |item|
         if item.is_a?(String)
           name = item
           default.deep_merge(
-            name: name,
-            configuration: { project_name: item },
+            Name: name,
+            Configuration: { ProjectName: item },
           )
         else # Hash
-          # With the hash notation, user needs to set: name and project_name
+          # With the hash notation, user needs to set: name and ProjectName
           #
-          #   codebuild(name: "action-name", project_name: "codebuild-project-names")
+          #   codebuild(Name: "action-name", ProjectName: "codebuild-project-names")
           #
-          project_name = item.delete(:project_name) || item[:name]
-          if project_name
-            item[:configuration] = { project_name: project_name }
+          project_name = item.delete(:ProjectName) || item[:Name]
+          if !item[:Configuration] && project_name
+            configuration = { ProjectName: project_name }
+            configuration[:PrimarySource] = item.delete(:PrimarySource) || default[:InputArtifacts].first[:Name]
+            item[:Configuration] = configuration
           end
 
-          item[:name] ||= project_name
+          item[:Name] ||= project_name
           item.reverse_merge(default)
         end
       end
